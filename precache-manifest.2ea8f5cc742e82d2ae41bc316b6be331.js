c1a06f31aaf3c0358855e59316a85cb41f82514d
Vladimir Gornyj <vladimir.gornyj@piano.io>
2019-01-18 00:01:11 +0300

    Updates

---
+++ precache-manifest.2ea8f5cc742e82d2ae41bc316b6be331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4705f544882ee29191f",
+    "url": "/text-for-instagram/static/js/runtime~main.e4705f54.js"
+  },
+  {
+    "revision": "4baf96b304d9474f3ad1",
+    "url": "/text-for-instagram/static/js/main.4baf96b3.chunk.js"
+  },
+  {
+    "revision": "6a4224a76730d73a044a",
+    "url": "/text-for-instagram/static/js/1.6a4224a7.chunk.js"
+  },
+  {
+    "revision": "6a4224a76730d73a044a",
+    "url": "/text-for-instagram/static/css/1.24052940.chunk.css"
+  },
+  {
+    "revision": "d87465aca2e67d73408df4a7c99da9e5",
+    "url": "/text-for-instagram/index.html"
+  }
+];
